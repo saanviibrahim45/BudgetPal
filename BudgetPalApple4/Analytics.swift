0306c25845f3045965c36037ae94648a47315c7a
saanviibrahim45 <123403592+saanviibrahim45@users.noreply.github.com>
2024-02-26 08:52:31 -0500

    Original working code (without the graphs taking in user input).

--- BudgetPalApple4/Analytics.swift
+++ BudgetPalApple4/Analytics.swift
@@ -14,52 +14,53 @@ class Analytics: UIViewController {
     @IBOutlet weak var barChartView: BarChartView!
 
     // Sample data for expenses (replace this with your actual data)
-    let expenseCategories = ["Food", "Clothing", "Entertainment", "Transportation", "Savings", "Education", "Bills", "Emergencies"]
-    let expenseValues = [200.0, 300.0, 150.0, 400.0, 200.0, 100.0,150.0, 50.0]
+        let expenseCategories = ["Food", "Clothing", "Entertainment", "Transportation", "Savings", "Education", "Bills", "Emergencies"]
+        let expenseValues = [200.0, 300.0, 150.0, 400.0, 200.0, 100.0,150.0, 50.0]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        override func viewDidLoad() {
+            super.viewDidLoad()
 
-        // Set up the bar chart
-        customizeBarChart()
+            // Set up the bar chart
+            customizeBarChart()
 
-        // Update the chart data
-        updateChartData()
-    }
-
-    func customizeBarChart() {
-        // Customize the appearance of the bar chart (optional)
-        barChartView.chartDescription.text = "Expense Categories"
-        barChartView.xAxis.labelPosition = .bottom
-        // Add more customization as needed
-    }
-
-    func updateChartData() {
-        // Create an array of BarChartDataEntry objects based on expense values
-        var dataEntries: [BarChartDataEntry] = []
-
-        for i in 0..<expenseValues.count {
-            let entry = BarChartDataEntry(x: Double(i), y: expenseValues[i])
-            dataEntries.append(entry)
+            // Update the chart data
+            updateChartData()
         }
 
-        // Create a BarChartDataSet with the data entries
-        let dataSet = BarChartDataSet(entries: dataEntries, label: " ")
-        
-        // Add labels for each expense category to the legend
-            dataSet.valueFont = UIFont.systemFont(ofSize: 12) // Set label text font
-        
-        // Set labels for x-axis (expense categories)
-        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: expenseCategories)
-        barChartView.xAxis.labelRotationAngle = -90 // Rotate labels vertically
+        func customizeBarChart() {
+            // Customize the appearance of the bar chart (optional)
+            barChartView.chartDescription.text = "Expense Categories"
+            barChartView.xAxis.labelPosition = .bottom
+            // Add more customization as needed
+        }
 
-        // Customize the appearance of the data set (colors, etc.)
-        dataSet.colors = ChartColorTemplates.vordiplom()
+        func updateChartData() {
+            // Create an array of BarChartDataEntry objects based on expense values
+            var dataEntries: [BarChartDataEntry] = []
 
-        // Create a BarChartData object with the data set
-        let data = BarChartData(dataSet: dataSet)
+            for i in 0..<expenseValues.count {
+                let entry = BarChartDataEntry(x: Double(i), y: expenseValues[i])
+                dataEntries.append(entry)
+            }
 
-        // Set data to the bar chart view
-        barChartView.data = data
+            // Create a BarChartDataSet with the data entries
+            let dataSet = BarChartDataSet(entries: dataEntries, label: " ")
+            
+            // Add labels for each expense category to the legend
+                dataSet.valueFont = UIFont.systemFont(ofSize: 12) // Set label text font
+            
+            // Set labels for x-axis (expense categories)
+            barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: expenseCategories)
+            barChartView.xAxis.labelRotationAngle = -90 // Rotate labels vertically
+
+            // Customize the appearance of the data set (colors, etc.)
+            dataSet.colors = ChartColorTemplates.vordiplom()
+
+            // Create a BarChartData object with the data set
+            let data = BarChartData(dataSet: dataSet)
+
+            // Set data to the bar chart view
+            barChartView.data = data
+        }
     }
-}
+     
--- BudgetPalApple4/ViewController.swift
+++ BudgetPalApple4/ViewController.swift
@@ -50,82 +50,85 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 
     func showTransactionPopup(isExpense: Bool) {
-        let alertController = UIAlertController(title: isExpense ? "Log Expense" : "Add Money", message: nil, preferredStyle: .alert)
+           let alertController = UIAlertController(title: isExpense ? "Log Expense" : "Add Money", message: nil, preferredStyle: .alert)
 
-        alertController.addTextField { (textField) in
-            textField.placeholder = "Amount"
-            textField.keyboardType = .decimalPad
-        }
-        alertController.addTextField { (textField) in
-            textField.placeholder = "Name"
-        }
-        alertController.addTextField { (textField) in
-            textField.placeholder = "Category"
-        }
+           alertController.addTextField { (textField) in
+               textField.placeholder = "Amount"
+               textField.keyboardType = .decimalPad
+           }
+           alertController.addTextField { (textField) in
+               textField.placeholder = "Name"
+           }
+           alertController.addTextField { (textField) in
+               textField.placeholder = "Category"
+           }
 
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        alertController.addAction(UIAlertAction(title: "Save", style: .default) { [weak self] _ in
-            if let amountText = alertController.textFields?[0].text,
-               let name = alertController.textFields?[1].text,
-               let category = alertController.textFields?[2].text,
-               let amount = Double(amountText) {
+           alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+           alertController.addAction(UIAlertAction(title: "Save", style: .default) { [weak self] _ in
+               if let amountText = alertController.textFields?[0].text,
+                  let name = alertController.textFields?[1].text,
+                  let category = alertController.textFields?[2].text,
+                  let amount = Double(amountText) {
 
-                let transaction = Transaction(amount: amount, name: name, category: category, isExpense: isExpense, date: Date())
-                self?.transactions.append(transaction)
-                self?.transactionTable.reloadData()
+                   let transaction = Transaction(amount: amount, name: name, category: category, isExpense: isExpense, date: Date())
+                   self?.transactions.append(transaction)
+                   self?.transactionTable.reloadData()
 
-                if isExpense {
-                    self?.updateCurrentBudget(with: -amount)
-                } else {
-                    self?.updateCurrentBudget(with: amount)
-                }
-            }
-        })
+                   if isExpense {
+                       self?.updateCurrentBudget(with: -amount)
+                   } else {
+                       self?.updateCurrentBudget(with: amount)
+                   }
+               }
+           })
 
-        self.present(alertController, animated: true, completion: nil)
-    }
+           self.present(alertController, animated: true, completion: nil)
+       }
 
-    func updateCurrentBudget(with amount: Double) {
-        if let currentBudgetText = currentBudgetAmount.text, let currentBudget = Double(currentBudgetText) {
-            let updatedBudget = currentBudget + amount
-            currentBudgetAmount.text = String(format: "%.2f", updatedBudget)
-        }
-    }
+       func updateCurrentBudget(with amount: Double) {
+           if let currentBudgetText = currentBudgetAmount.text, let currentBudget = Double(currentBudgetText) {
+               let updatedBudget = currentBudget + amount
+               currentBudgetAmount.text = String(format: "%.2f", updatedBudget)
+           }
+       }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return transactions.count
-    }
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return transactions.count
+       }
+       
+       
 
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)
-//        let transaction = transactions[indexPath.row]
-//        cell.textLabel?.text = "\(transaction.isExpense ? "-" : "+") \(transaction.amount) (\(transaction.name), \(transaction.category))"
-//        return cell
-//    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)
-        let transaction = transactions[indexPath.row]
+   //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   //        let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)
+   //        let transaction = transactions[indexPath.row]
+   //        cell.textLabel?.text = "\(transaction.isExpense ? "-" : "+") \(transaction.amount) (\(transaction.name), \(transaction.category))"
+   //        return cell
+   //    }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)
+           let transaction = transactions[indexPath.row]
 
-        let amountText: String
-        if transaction.isExpense {
-            cell.textLabel?.textColor = UIColor.red
-            amountText = "-\(transaction.amount)"
-        } else {
-            cell.textLabel?.textColor = UIColor.green
-            amountText = "+\(transaction.amount)"
-        }
+           let amountText: String
+           if transaction.isExpense {
+               cell.textLabel?.textColor = UIColor.red
+               amountText = "-\(transaction.amount)"
+           } else {
+               cell.textLabel?.textColor = UIColor.green
+               amountText = "+\(transaction.amount)"
+           }
 
-        cell.textLabel?.text = "\(amountText) (\(transaction.name), \(transaction.category))"
-        return cell
-    }
+           cell.textLabel?.text = "\(amountText) (\(transaction.name), \(transaction.category))"
+           return cell
+       }
 
-}
+   }
 
-struct Transaction {
-    let amount: Double
-    let name: String
-    let category: String
-    let isExpense: Bool
-    let date: Date
-}
+   struct Transaction {
+       let amount: Double
+       let name: String
+       let category: String
+       let isExpense: Bool
+       let date: Date
+   }
+
